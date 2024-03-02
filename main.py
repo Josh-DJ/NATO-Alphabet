2a8074ccb042e0ac2aa142c7575bb68031c5779b
Joshua Jimenez <jjimen7m20@gmail.com>
2024-03-02 18:05:50 -0500

    Added loop.

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-usr = input("Enter word: ").upper()
-
-try:
-    result = [nato_dict[letter] for letter in usr]
-except KeyError:
-    print("Please input a letter.")
-else:
-    print(result)
+flag = False
+while flag == False:
+    usr = input("Enter word: ").upper()
+    if usr.isalpha():
+        flag = True
+    try:
+        result = [nato_dict[letter] for letter in usr]
+    except KeyError:
+        print("Please input a letter.")
+    else:
+        print(result)
