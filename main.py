8a9acb7d33c65c0f09694f77dcfdbf005b3c0ce8
Joshua Jimenez <jjimen7m20@gmail.com>
2024-03-02 17:34:53 -0500

    Updated to include exception handling.

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 usr = input("Enter word: ").upper()
-result = [nato_dict.get(letter) for letter in usr]
-print(result)
+
+try:
+    result = [nato_dict.get(letter) for letter in usr]
+except KeyError:
+    print("Please input a letter.")
+else:
+    print(result)
